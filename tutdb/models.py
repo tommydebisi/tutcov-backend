0b1cfc5a589e677862644467072ae8e648c9b42b
prosper <edwardprosper001@gmail.com>
2023-10-09 15:54:22 +0100

    Added is_superuser field

--- tutdb/models.py
+++ tutdb/models.py
@@ -18,6 +18,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     department = models.CharField(max_length=100, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     objects = UserManager()
@@ -77,7 +78,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['password']  # Add 'password' to the REQUIRED_FIELDS list
+    REQUIRED_FIELDS = ['username', 'password']  # Add 'password' to the REQUIRED_FIELDS list
 
     def __str__(self):
         return self.email
