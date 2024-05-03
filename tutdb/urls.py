8ad31a3597cb9436974099f36a91e41c60853e7e
prosper <edwardprosper001@gmail.com>
2024-05-03 22:57:52 +0100

    'updates'

--- tutdb/urls.py
+++ tutdb/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("all/", QuestionListApiView.as_view(), name="all-questions"),
     path("<uuid:uuid>/", QuestionDetailAPIView.as_view(), name='single-question'),
     path("<str:session>/<str:course_code>/", CourseQuestions.as_view(), name="course-questions"), 
-    
 
 ]
